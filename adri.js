349a198af113d9a72eb56026147ed985eba01c73
Adrienchester <43817445+Adrienchester@users.noreply.github.com>
2023-10-31 11:12:09 +0100

    Update adri.js

--- adri.js
+++ adri.js
@@ -199,7 +199,7 @@ import { Mesh, Program, Texture } from 'ogl'
 import fragment from './fragment.glsl'
 import vertex from './vertex.glsl'
 
-export default class Media {
+export class Media {
   constructor ({ element, geometry, gl, height, scene, screen, viewport }) {
     this.element = element
     this.image = this.element.querySelector('img')
